cc4a42663e23655d848069c450422900fc7bb346
ChansooKim316 <cccssk@naver.com>
2021-06-23 19:51:58 +0900

    0623 7:51pm

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,5 @@
 
-const handleProfileGet = (req, res)=>{
+const handleProfileGet = (req, res, db) => {
 	const { id } = req.params;
 	// if property == value (id: id), you can write just once. (ES6 feature)
 	db.select('*').from('users').where({id})
@@ -13,7 +13,25 @@ const handleProfileGet = (req, res)=>{
 		.catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+	const { id } = req.params;
+	const { name, age, pet} = req.body.formInput;
+	db('users')
+		.where({ id }) // the id that matches to 'const id = req.params'
+		.update({ name: name })
+		.then(resp => {
+			if (resp) {
+				res.json('Success')
+			} else {
+				res.status(400).json('Unable to update')
+			}
+		})
+		.catch(err => res.status(400).json('Error updating user'))
+}
+
+
 // ES6 : when key = valuew, write it once.
 module.exports = {
-	handleProfileGet
+	handleProfileGet,
+	handleProfileUpdate
 }
